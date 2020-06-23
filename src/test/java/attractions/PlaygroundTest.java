dac050895f410244fd5d5bd651117d3bdbce890b
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-23 19:32:13 +0100

    add tests to make sure anyone over 15 cant access the playground and anyone who is 15 can access it. both tests pass

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -34,4 +34,16 @@ public class PlaygroundTest {
         Visitor visitor = new Visitor(12, 1.1, 2.50);
         assertEquals(true, playground.isAllowedTo(visitor));
     }
+
+    @Test
+    public void visitorIsNotAllowedInThePlayground(){
+        Visitor visitor = new Visitor(16, 1.6, 6.0);
+        assertEquals(false, playground.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void visitorWhoIs15IsAllowedInThePlayground(){
+        Visitor visitor = new Visitor(15, 1.5, 1.30);
+        assertEquals(true, playground.isAllowedTo(visitor));
+    }
 }
