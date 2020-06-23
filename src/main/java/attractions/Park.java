25c793dbe81758a198e6b2db9fac8c6fe89fe840
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-23 20:40:37 +0100

    make the attractions implement the IReviewed interface

--- src/main/java/attractions/Park.java
+++ src/main/java/attractions/Park.java
@@ -1,8 +1,6 @@
 package attractions;
 
-import behaviours.IReviewed;
-
-public class Park extends Attraction implements IReviewed {
+public class Park extends Attraction {
 
     public Park(String name, int rating) {
         super(name, rating);
